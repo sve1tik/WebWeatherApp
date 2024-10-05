c2671c0e6d679c0e0db50c241ca1a59d2d9e0ae3
sve1tik <nikitacerkas131@gmail.com>
2024-10-05 19:38:31 +0300

    fix

--- src/getWeather.js
+++ src/getWeather.js
@@ -10,39 +10,43 @@ const $api = axios.create({
 })
 
 export async function getWeather(city) {
-  let lat = 0
-  let lon = 0
-  let icon = ''
-  let desc = ''
-  let current_temp = 0
-  let max_temp = 0
-  let min_temp = 0
-  let wind_speed = 0
-  let gust = 0
+  const weatherData = {
+    lat: 0,
+    lon: 0,
+    icon: '',
+    desc: '',
+    current_temp: 0,
+    max_temp: 0,
+    min_temp: 0,
+    wind_speed: 0,
+    gust: 0
+  }
   await $api
     .get(`geo/1.0/direct?q=${city}&limit=1&appid=${getENV('VITE_API_KEY')}`)
     .then((response) => {
-      lat = response.data[0].lat
-      lon = response.data[0].lon
+      weatherData.lat = response.data[0].lat
+      weatherData.lon = response.data[0].lon
     })
   await $api
-    .get(`data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${getENV('VITE_API_KEY')}`)
+    .get(
+      `data/2.5/weather?lat=${weatherData.lat}&lon=${weatherData.lon}&units=metric&appid=${getENV('VITE_API_KEY')}`
+    )
     .then((response) => {
-      icon = response.data.weather[0].icon
-      desc = response.data.weather[0].description
-      current_temp = response.data.main.temp
-      max_temp = response.data.main.temp_max
-      min_temp = response.data.main.temp_min
-      wind_speed = response.data.wind.speed
-      gust = response.data.wind.gust
+      weatherData.icon = response.data.weather[0].icon
+      weatherData.desc = response.data.weather[0].description
+      weatherData.current_temp = response.data.main.temp
+      weatherData.max_temp = response.data.main.temp_max
+      weatherData.min_temp = response.data.main.temp_min
+      weatherData.wind_speed = response.data.wind.speed
+      weatherData.gust = response.data.wind.gust
     })
   return {
-    icon: icon,
-    desc: desc,
-    current_temp: current_temp,
-    min_temp: min_temp,
-    max_temp: max_temp,
-    wind_speed: wind_speed,
-    gust: gust
+    icon: weatherData.icon,
+    desc: weatherData.desc,
+    current_temp: weatherData.current_temp,
+    min_temp: weatherData.min_temp,
+    max_temp: weatherData.max_temp,
+    wind_speed: weatherData.wind_speed,
+    gust: weatherData.gust
   }
 }
